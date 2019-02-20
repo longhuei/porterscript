7f2fad46d29634525cc268dda89790a07e6f0a0a
longhuei <longhuei@g.ecc.u-tokyo.ac.jp>
2019-02-20 13:28:11 +0900

    Fix

--- porterscript.py
+++ porterscript.py
@@ -1,8 +1,14 @@
 import sys
+import string
 from porter2stemmer import Porter2Stemmer
 
 stemmer = Porter2Stemmer()
+table = str.maketrans({key: None for key in string.punctuation})
 
 for line in sys.stdin:
-    stemmed = stemmer.stem(line.rstrip())
-    print(stemmed)
+    words = line.rstrip().split()
+    for w in words:
+        w = w.translate(table)  # removes punctuation
+        print(stemmer.stem(w) + ' ', end='')
+
+    print("")
